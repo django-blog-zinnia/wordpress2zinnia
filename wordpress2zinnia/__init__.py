0cec5cc2c3d036175d7918de6a02bbf14e388654
Fantomas42 <fantomas42@gmail.com>
2015-01-14 16:39:17 +0100

    Create module

---
+++ wordpress2zinnia/__init__.py
@@ -0,0 +1,8 @@
+"""wordpress2zinnia"""
+__version__ = '1.0'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/django-blog-zinnia/wordpress2zinnia'
